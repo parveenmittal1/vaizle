20c96d6a98f9ba1051a5a4042460cf254b808a68
parveen <mittalparveen652@gmail.com>
2020-06-08 11:34:24 +0530

    functions ready

---
+++ App.js
@@ -0,0 +1,29 @@
+const getDatesBetweenDates = (startDate, endDate) => {
+    let dates = []
+    //to avoid modifying the original date
+    const theDate = new Date(startDate)
+    while (theDate < endDate) {
+        dates = [...dates, new Date(theDate)]
+        theDate.setDate(theDate.getDate() + 1)
+    }
+    return dates
+}
+
+// new Date("dateString") is browser-dependent and discouraged, so we'll write
+// a simple parse function for U.S. date format (which does no error checking)
+function parseDate(str) {
+    var mdy = str.split('-');
+    return new Date(mdy[2], mdy[0]-1, mdy[1]);
+}
+
+function datediff(first, second) {
+    // Take the difference between the dates and divide by milliseconds per day.
+    // Round to nearest whole number to deal with DST.
+    return Math.round((second-first)/(1000*60*60*24));
+}
+
+function solution(D){
+    //
+
+    return D;
+}();
